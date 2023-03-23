e5b0b34d60aa2285cec26f5861497d4495789443
krhamby <hambykr19@gcc.edu>
2023-03-23 14:17:32 -0400

    Added a constructor test for GameResult

--- test/GameResultTest.java
+++ test/GameResultTest.java
@@ -2,5 +2,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameResultTest {
-  
+    @Test
+    void constructorTest() {
+        GameResult result = new GameResult(true, 1, 5);
+        assertEquals(true, result.humanWasPlaying);
+        assertEquals(1, result.correctValue);
+        assertEquals(5, result.numGuesses);
+    }
 }
