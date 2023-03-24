b85757f954e5c3efec08784c29fd92399394afe3
krhamby <hambykr19@gcc.edu>
2023-03-24 14:55:46 -0400

    Create Random testing double

--- test/RandomTestingDouble.java
+++ test/RandomTestingDouble.java
@@ -1,3 +1,17 @@
-class RandomTestingDouble {
-  
+import java.util.Random;
+
+class RandomTestingDouble extends Random {
+    private int index;
+    private int[] values;
+
+    public RandomTestingDouble() {
+        index = 0;
+        values = new int[]{47, 134, 283, 355, 400, 591, 702, 736, 852, 988};
+    }
+
+    public int nextInt(int bound) {
+        int value = values[index];
+        index = (index + 1) % values.length;
+        return value;
+    } 
 }
